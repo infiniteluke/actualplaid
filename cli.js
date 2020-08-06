f7c9feb5cc829be0b57811d555bc1020e8e4a735
Luke Herrington <luke.herrington@appfolio.com>
2020-08-05 23:39:50 -0700

    Initial working actualplaid prototype

---
+++ cli.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+
+const meow = require("meow");
+const actualPlaid = require("./index");
+
+const cli = meow(
+  `
+	Usage
+	  $ actualplaid <command> <flags>
+
+	Commands & Options
+    setup            Link bank accounts with your Actual Budget accounts via Plaid
+    import           Sync bank accounts to Actual Budget
+	    --account, -a   The account to import, ex: --account="My Checking"
+	    --since, -s     The start date after which transactions should be imported, format: yyyy-MM-dd, ex: --since=2020-05-28
+    config           Print the location of actualplaid the config file
+
+	Examples
+	  $ actualplaid import --account="My Checking" --since="2020-05-28"
+`,
+  {
+    flags: {
+      reset: {
+        alias: "r",
+        type: "string",
+      },
+      account: {
+        alias: "a",
+        type: "string",
+      },
+      since: {
+        alias: "s",
+        type: "string",
+      },
+    },
+  }
+);
+
+actualPlaid(cli.input[0], cli.flags);
---
+++ index.js
@@ -0,0 +1,296 @@
+const util = require("util");
+const plaid = require("plaid");
+const path = require("path");
+const fastify = require("fastify")({ logger: { level: "fatal" } });
+const actual = require("@actual-app/api");
+const opn = require("better-opn");
+const dateFns = require("date-fns");
+const inquirer = require("inquirer");
+const terminalLink = require("terminal-link");
+const Conf = require("conf");
+
+const config = new Conf();
+let syncingData;
+
+fastify.register(require("fastify-static"), {
+  root: path.join(__dirname, "public"),
+  prefix: "/public/",
+});
+
+const start = async () => {
+  try {
+    await fastify.listen(APP_PORT);
+    // fastify.log.info(`server listening on ${fastify.server.address().port}`);
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+
+const ACTUAL_BUDGET_ID = process.env.ACTUAL_BUDGET_ID;
+
+const APP_PORT = process.env.APP_PORT || 3000;
+
+const PLAID_CLIENT_ID = process.env.PLAID_CLIENT_ID;
+const PLAID_SECRET = process.env.PLAID_SECRET;
+const PLAID_ENV = process.env.PLAID_ENV || "development";
+var PLAID_PRODUCTS = (process.env.PLAID_PRODUCTS || "transactions").split(",");
+var PLAID_COUNTRY_CODES = (process.env.PLAID_COUNTRY_CODES, "US").split(",");
+
+var client = new plaid.Client({
+  clientID: PLAID_CLIENT_ID,
+  secret: PLAID_SECRET,
+  env: plaid.environments[PLAID_ENV],
+  options: {
+    version: "2019-05-29",
+  },
+});
+
+const getTransactions = util.promisify(client.getTransactions);
+const getAccounts = util.promisify(client.getAccounts);
+
+const prettyPrintResponse = (response) => {
+  console.log(util.inspect(response, { colors: true, depth: 4 }));
+};
+
+const transactionMapper = (accountId) => (transaction) => ({
+  account_id: accountId,
+  date: transaction.date,
+  amount: transaction.amount,
+  amount: transaction.amount,
+  payee: transaction.merchant_name || transaction.name,
+  imported_payee: transaction.merchant_name || transaction.name,
+  imported_id: transaction.transaction_id,
+  location: transaction.pending,
+  pending: transaction.pending,
+  cleared: !transaction.pending,
+});
+
+module.exports = async (command, flags) => {
+  if (!command) {
+    console.log('Try "actualplaid --help"');
+    process.exit();
+  }
+  try {
+    await actual.init();
+    await actual.loadBudget(ACTUAL_BUDGET_ID);
+    syncingData = config.get(`actualSync`) || {};
+  } catch (e) {
+    console.error(`Could not load Actual: ${e.message}`);
+    process.exit(1);
+  }
+  if (command === "config") {
+    console.log(config.path);
+    process.exit();
+  } else if (command === "import") {
+    if (Object.keys(syncingData).length) {
+      try {
+        const accountsToSync = Object.entries(syncingData).filter(
+          ([_, account]) =>
+            !flags["account"] || account.actualName === flags["account"]
+        );
+
+        for (let [actualId, account] of accountsToSync) {
+          const startDate = dateFns.format(
+            new Date(
+              flags["since"] ||
+                account.lastImport ||
+                dateFns.subMonths(new Date(), 1)
+            ),
+            "yyyy-MM-dd"
+          );
+          const endDate = dateFns.format(new Date(), "yyyy-MM-dd");
+          const transactionsResponse = await getTransactions.call(
+            client,
+            account.plaidToken,
+            startDate,
+            endDate,
+            {}
+          );
+
+          await actual.importTransactions(
+            actualId,
+            transactionsResponse.transactions
+              .filter(
+                (transaction) =>
+                  transaction.account_id === account.plaidAccount.account_id
+              )
+              .map(transactionMapper(actualId))
+          );
+          config.set(`actualSync.${actualId}.lastImport`, new Date());
+        }
+        console.log("Import completed");
+        process.exit();
+      } catch (e) {
+        prettyPrintResponse(e);
+        process.exit(1);
+      }
+    } else {
+      console.error(
+        "No Plaid links found. Please run `actualplaid setup` before trying to import"
+      );
+      process.exit(1);
+    }
+  } else if (command === "setup") {
+    const actualAccounts = await actual.getAccounts();
+    const { dissmissedWarning } = await inquirer.prompt({
+      type: "confirm",
+      name: "dissmissedWarning",
+      message: `WARNING: A Plaid Dev account has a limited number of Links. See the ${terminalLink(
+        "Plaid Development Dashboard",
+        "https://dashboard.plaid.com/overview/development"
+      )} to check your usage. Proceed?`,
+    });
+    if (dissmissedWarning) {
+      start();
+      const { correctBudget } = await inquirer.prompt({
+        type: "confirm",
+        name: "correctBudget",
+        message: `You are about to setup Plaid Links for the following Actual Budget File: ${ACTUAL_BUDGET_ID}. Proceed?`,
+      });
+      if (correctBudget) {
+        const { accountsToSync } = await inquirer.prompt({
+          type: "checkbox",
+          name: "accountsToSync",
+          message: "Which accounts do you want to sync with plaid?",
+          choices: actualAccounts.map(({ name, id }) => ({ name, value: id })),
+        });
+
+        const { confirm } = await inquirer.prompt({
+          type: "confirm",
+          name: "confirm",
+          message: `A browser window will now open. Please link each bank you expect to sync with Actual. Proceed?`,
+        });
+
+        if (!confirm) {
+          process.exit(1);
+        }
+
+        const plaidLinkLink = `http://localhost:${APP_PORT}`;
+        console.log(
+          `Opening ${plaidLinkLink} to link with Plaid...\nNOTE: Please return to your CLI when completed.`
+        );
+        opn(plaidLinkLink);
+
+        const { doneLinking } = await inquirer.prompt({
+          type: "confirm",
+          name: "doneLinking",
+          message: `Are you done linking banks?`,
+        });
+        const plaidAccounts = config.get("plaidAccounts");
+        if (!plaidAccounts) {
+          console.log("You did not Link any Plaid accounts");
+          process.exit(1);
+        }
+
+        for (acctId of accountsToSync) {
+          actualAcct = actualAccounts.find((a) => a.id === acctId);
+          console.log(plaidAccounts);
+          let syncChoices = Object.values(plaidAccounts).map(
+            ({ account, plaidBankName }) => ({
+              value: account.account_id,
+              name: `${plaidBankName}: ${account.name} - ${account.subtype}/${account.type} (${account.mask})`,
+            })
+          );
+          const { plaidAccountToSync } = await inquirer.prompt({
+            type: "list",
+            name: "plaidAccountToSync",
+            message: `Which Plaid acount do you want to sync with "${actualAcct.name}"?`,
+            choices: syncChoices,
+          });
+          console.log({ plaidAccountToSync });
+          const plaidAccount = Object.values(plaidAccounts).find(
+            ({ account }) => account.account_id === plaidAccountToSync
+          );
+          console.log({ plaidAccount });
+
+          config.set(`actualSync.${acctId}`, {
+            actualName: actualAcct.name,
+            actualType: actualAcct.type,
+            actualAccountId: actualAcct.id,
+            plaidItemId: plaidAccount.plaidItemId,
+            plaidToken: plaidAccount.plaidToken,
+            plaidAccount: plaidAccount.account,
+            plaidBankName: plaidAccount.plaidBankName,
+          });
+        }
+        console.table(
+          Object.values(config.get("actualSync")).map((account) => ({
+            "Actual Account": account.actualName,
+            "Actual Type": account.actualType,
+            "Plaid Bank": account.plaidBankName,
+            "Plaid Account": account.plaidAccount.name,
+            "Plaid Type": `${account.plaidAccount.subtype}/${account.plaidAccount.type}`,
+            "Plaid Account #": account.plaidAccount.mask,
+          }))
+        );
+      }
+      console.log(
+        `Setup completed sucessfully. Run \`actualplaid import\` to sync your setup banks with their respective actual accounts`
+      );
+      process.exit();
+    } else {
+      console.error(`Setup was not run`);
+      process.exit();
+    }
+  }
+};
+
+fastify.get("/", (requst, reply) => reply.sendFile("index.html"));
+
+fastify.post("/create_link_token", (request, reply) => {
+  const configs = {
+    user: { client_user_id: "user-id" },
+    client_name: "Actual Budget Plaid Importer",
+    products: PLAID_PRODUCTS,
+    country_codes: PLAID_COUNTRY_CODES,
+    language: "en",
+  };
+  client.createLinkToken(configs, function (error, { link_token }) {
+    if (error != null) {
+      prettyPrintResponse(error);
+      return;
+    }
+    reply.send({ link_token });
+  });
+});
+
+fastify.post("/get_access_token", (request, reply) => {
+  const body = JSON.parse(request.body);
+  client.exchangePublicToken(body.public_token, function (
+    error,
+    tokenResponse
+  ) {
+    if (error != null) {
+      prettyPrintResponse(error);
+      return;
+    }
+    client.getAccounts(
+      tokenResponse.access_token,
+      async (error, { accounts, item }) => {
+        if (error != null) {
+          prettyPrintResponse(error);
+          return;
+        }
+        client.getInstitutionById(
+          item.institution_id,
+          (error, { institution }) => {
+            if (error != null) {
+              prettyPrintResponse(error);
+              return;
+            }
+            accounts.forEach((account) => {
+              config.set(`plaidAccounts.${account.account_id}`, {
+                account,
+                plaidToken: tokenResponse.access_token,
+                plaidItemId: tokenResponse.item_id,
+                plaidBankName: institution.name,
+              });
+            });
+            reply.send({ ok: true });
+          }
+        );
+      }
+    );
+  });
+});
